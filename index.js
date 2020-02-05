84e80eef0dbd0d252fd49a78b4bbb241d4262c2a
Ty Schenk <7807169+schenkty@users.noreply.github.com>
2020-02-05 01:06:10 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+// eslint-disable-next-line import/no-commonjs, import/no-unassigned-import
+require('babel-register');
+
+// eslint-disable-next-line import/no-commonjs
+const { recoverWallet } = require('./src/index');
+
+recoverWallet();
